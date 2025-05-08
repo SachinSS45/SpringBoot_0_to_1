31aff5422d33cf5da661e92bf2dd4a08ed5b407b
Sachin Shetkar <ssshetkar2001@gmail.com>
2025-05-08 12:59:44 +0530

    Chapter 2.1

---
+++ Week02_SpringMVC_RestAPIs/SpringWebMVC/src/main/java/com/sachin/SpringWebMVC/SpringWebMvcApplication.java
@@ -0,0 +1,14 @@
+package com.sachin.SpringWebMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringWebMvcApplication.class, args);
+	}
+
+}
